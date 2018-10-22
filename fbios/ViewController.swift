212c980fb0bd5aacde50c41d543f8cf881865c30
huynhan47 <huynxuanan@posco.net>
2018-10-22 15:36:17 +0700

    Initial Commit

---
+++ fbios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fbios
+//
+//  Created by Posco ICT VN on 10/22/18.
+//  Copyright © 2018 Posco ICT VN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
